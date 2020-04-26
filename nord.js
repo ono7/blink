bce073291a2410253dfb4aa8bdd46bed4a136a15
lima <lima@mbp>
2020-04-26 00:22:49 -0500

    updates

---
+++ nord.js
@@ -0,0 +1,29 @@
+
+black         =  '#2e3440'
+red           =  '#bf616a'
+green         =  '#a3be8c'
+yellow        =  '#ebcb8b'
+blue          =  '#81a1c1'
+magenta       =  '#b48ead'
+cyan          =  '#88c0d0'
+white         =  '#d8dee9'
+lightBlack    =  '#7c8595'
+lightRed      =  '#bf616a'
+lightGreen    =  '#a3be8c'
+lightYellow   =  '#ebcb8b'
+lightBlue     =  '#81a1c1'
+lightMagenta  =  '#b48ead'
+lightCyan     =  '#88c0d0'
+lightWhite    =  '#eceff4'
+
+t.prefs_.set('color-palette-overrides', [
+  black, red, green, yellow, blue, magenta, cyan, white,
+  lightBlack, lightRed, lightGreen, lightYellow, lightBlue, lightMagenta, lightCyan, lightWhite
+]);
+
+
+t.prefs_.set('cursor-color', 'rgba(215,255,0,0.3)');
+t.prefs_.set('cursor-blink', false);
+
+t.prefs_.set('foreground-color', white);
+t.prefs_.set('background-color', black);
