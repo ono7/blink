12f37957008cf801f4d696a33c592d368d349472
lima <lima@mbp>
2020-04-30 01:40:13 -0500

    updates

---
+++ gruvbox.js
@@ -0,0 +1,28 @@
+black         =  '#282828'
+red           =  '#ea6962'
+green         =  '#a9b665'
+yellow        =  '#e78a4e'
+blue          =  '#7daea3'
+magenta       =  '#d3869b'
+cyan          =  '#89b482'
+white         =  '#dfbf8e'
+lightBlack    =  '#7c8595'
+lightRed      =  '#ea6962'
+lightGreen    =  '#a9b665'
+lightYellow   =  '#e3a84e'
+lightBlue     =  '#7daea3'
+lightMagenta  =  '#d3869b'
+lightCyan     =  '#89b482'
+lightWhite    =  '#dfbf8e'
+
+t.prefs_.set('color-palette-overrides', [
+  black, red, green, yellow, blue, magenta, cyan, white,
+  lightBlack, lightRed, lightGreen, lightYellow, lightBlue, lightMagenta, lightCyan, lightWhite
+]);
+
+
+t.prefs_.set('cursor-color', 'rgba(215,255,0,0.3)');
+t.prefs_.set('cursor-blink', false);
+
+t.prefs_.set('foreground-color', white);
+t.prefs_.set('background-color', black);
